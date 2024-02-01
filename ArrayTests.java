5d533ed2dd08a3a44847e43fc79e591b25cbf7a3
Sarah Li <100035287+sli051357@users.noreply.github.com>
2024-02-01 12:59:47 -0800

    update

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
 import org.junit.*;
 
 public class ArrayTests {
@@ -14,5 +17,18 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test 
+  public void testReverseInPlaceActual() {
+    int[] input1 = {1, 2, 3};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{3, 2, 1}, input1);
+  }
+
+  @Test 
+  public void testReversedActual() {
+    int[] input1 = {1, 2, 3};
+    assertArrayEquals(new int[]{3, 2, 1}, ArrayExamples.reversed(input1));
   }
 }
