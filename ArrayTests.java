8a1a8882d170e58365f12ea71ccef81174498317
Sarah Li <100035287+sli051357@users.noreply.github.com>
2024-02-01 13:08:32 -0800

    update

--- ArrayTests.java
+++ ArrayTests.java
@@ -31,4 +31,28 @@ public class ArrayTests {
     int[] input1 = {1, 2, 3};
     assertArrayEquals(new int[]{3, 2, 1}, ArrayExamples.reversed(input1));
   }
+
+  /**
+   * Tests averageWithoutLowest() when the input is less than 2
+   */
+  @Test 
+  public void testAverageWithoutLowestSmall() {
+    double[] input1 = {};
+    double[] input2 = {1.0};
+
+    assertEquals(0.0, ArrayExamples.averageWithoutLowest(input1), 0.0);
+    assertEquals(0.0, ArrayExamples.averageWithoutLowest(input1), 0.0);
+  }
+
+  /**
+   * Tests averageWithoutLowest() with a larger input array
+   */
+  @Test 
+  public void testAverageWithoutLowest() {
+    double[] input1 = {1.0, 2.0, 3.0, 4.0, 5.0};
+    double[] input2 = {2.0, 0.0, 3.0, 5.0, 4.0};
+
+    assertEquals(3.5, ArrayExamples.averageWithoutLowest(input1), 0.0);
+    assertEquals(3.5, ArrayExamples.averageWithoutLowest(input2), 0.0);
+  }
 }
